0f630b7c86b11d4ab5206b3f4c72c8ef8356e130
Ivo Donchev <i.donchev@hacksoft.io>
2022-06-21 12:05:25 +0300

    Add demo_1

--- django/app/app/urls.py
+++ django/app/app/urls.py
@@ -7,6 +7,8 @@ from .views import (
     view_with_multiprocessing,
     demo,
     demo_async,
+    demo_1_sync,
+    demo_1_async,
 )
 
 
@@ -17,4 +19,6 @@ urlpatterns = [
     path('async/', async_view),
     path('demo/', demo),
     path('demo/async/', demo_async),
+    path('demo/1/sync/', demo_1_sync),
+    path('demo/1/async/', demo_1_async),
 ]
--- django/app/app/views/__init__.py
+++ django/app/app/views/__init__.py
@@ -2,3 +2,5 @@ from .multiprocessing import *
 from .multithreading import *
 from .async_view import *
 from .demo import *
+from .demo_1 import *
+from .demo_2 import *
